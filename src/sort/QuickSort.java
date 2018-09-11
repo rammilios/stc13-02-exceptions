e304cbb7c28ca8a2631bb0442027c2a1a3d91a28
rammilios <ram7260@yandex.ru>
2018-09-11 18:28:26 +0300

    Initial commit

---
+++ src/sort/QuickSort.java
@@ -0,0 +1,19 @@
+package sort;
+
+class QuickSort
+{
+    public static void main(String[] args)
+    {
+        int maxSize = 16;
+        ArrayIns arr;
+        arr = new ArrayIns(maxSize);
+        for(int j=0; j<maxSize; j++)
+        {
+            long n = (int)(java.lang.Math.random()*99);
+            arr.insert(n);
+        }
+        arr.display();
+        arr.quickSort();
+        arr.display();
+    }
+}
